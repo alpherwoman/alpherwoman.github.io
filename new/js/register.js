1a62e00f51105f799725a8311d5fe144ea5ab809
NATFINEST <ese.nat4@gmail.com>
2020-09-16 08:31:20 +0100

    up

--- new/js/register.js
+++ new/js/register.js
@@ -7,13 +7,26 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $(".form-control-name").on('input', function(){
-        if ($('.fname').val() == "" || $('.sname').val() == "" || $('.email').val() == "" || $('.phone').val() == ""){
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if ($('.fname').val() == "" || $('.sname').val() == "" || !$('.email').val().match(re) || $('.phone').val().length == 0 || $('.phone').val().length >= 15 ){
             $('.continue').addClass('cursor-auto') 
-            $('.continue').attr('disabled',true)   
+            $('.continue').attr('disabled',true)  
         }
         else{
             $('.continue').removeAttr('disabled')
             $('.continue').removeClass('cursor-auto') 
+        }
+    })
+
+    $(".form-control-two").on('change', function(){
+        if ($('.gender').select2('data')[0]['id'] == "" || $('.state').select2('data')[0]['id'] == "" || $('.job').select2('data')[0]['id'] == "" || $('.religion').select2('data')[0]['id'] == "" || $('.date').val() == ""){
+            
+            $('.submit').addClass('cursor-auto') 
+            $('.submit').attr('disabled',true)   
+        }
+        else{
+            $('.submit').removeAttr('disabled')
+            $('.submit').removeClass('cursor-auto') 
         }
     })
     
