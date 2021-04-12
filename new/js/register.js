8a45ce67b7d0c85d811d0d7c0bce93cd2c204aa3
NATFINEST <ese.nat4@gmail.com>
2021-04-12 01:03:52 +0100

    up

--- new/js/register.js
+++ new/js/register.js
@@ -78,6 +78,39 @@ $(document).ready(function(){
     })
 });
 
+$(document).ready(function(){
+    $(".form-control-profile").on('input', function(){
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if ($('.pfname').val() == "" || $('.psname').val() == "" || $('.password').val() == "" || !$('.pemail').val().match(re) || $('.pphone').val().length == 0 || $('.pphone').val().length >= 15 ){
+            $('.pcontinue').addClass('cursor-auto') 
+            $('.pcontinue').attr('disabled',true)  
+        }
+        else{
+            $('.pcontinue').removeAttr('disabled')
+            $('.pcontinue').removeClass('cursor-auto') 
+        }
+    })
+
+    $(".form-control-profile-two").on('change', function(){
+        if ($('.pgender').select2('data')[0]['id'] == "" || $('.pstate').select2('data')[0]['id'] == "" || $('.pjob').select2('data')[0]['id'] == "" || $('.preligion').select2('data')[0]['id'] == "" || $('.pdate').val() == ""){
+            
+            $('.psubmit').addClass('cursor-auto') 
+            $('.psubmit').attr('disabled',true)   
+        }
+        else{
+            $('.psubmit').removeAttr('disabled')
+            $('.psubmit').removeClass('cursor-auto') 
+        }
+    })
+    
+    $('.pcontinue').on('click', function(){
+        $(this).hide();
+        $('.psubmit').show();
+        $('.pfirst').hide();
+        $('.psecond').show();
+    })
+});
+
 $(document).ready(function() {
     $("body").on('click', '.toggle-password', function() {
       $(this).toggleClass("fa-eye fa-eye-slash");
