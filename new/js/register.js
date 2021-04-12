467396ff057f247dfafdf77525963c8442f48190
NATFINEST <ese.nat4@gmail.com>
2021-04-12 01:42:00 +0100

    up

--- new/js/register.js
+++ new/js/register.js
@@ -23,7 +23,7 @@ function deltaDate(input, days, months, years) {
 
 $(document).ready(function(){
     $(".create-password").on('input', function(){
-        if ($('.create-password.password').val() == "" || $('.create-password.password').val() != $('.create-password.confirm').val()){
+        if ($('.create-password.password').val() == "" || $('.create-password.password').val().length < 4 || $('.create-password.password').val() != $('.create-password.confirm').val()){
             $('.password-submit').addClass('cursor-auto') 
             $('.password-submit').attr('disabled',true)  
         }
@@ -37,7 +37,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".form-control-name").on('input', function(){
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        if ($('.fname').val() == "" || $('.sname').val() == "" || $('.password').val() == "" || !$('.email').val().match(re) || $('.phone').val().length == 0 || $('.phone').val().length >= 15 || $('.password').val() != $('.confirm').val()){
+        if ($('.fname').val() == "" || $('.sname').val() == "" || $('.password').val() == "" || $('.password').val().length < 4 || !$('.email').val().match(re) || $('.phone').val().length == 0 || $('.phone').val().length >= 15 || $('.password').val() != $('.confirm').val()){
             $('.continue').addClass('cursor-auto') 
             $('.continue').attr('disabled',true)  
         }
@@ -81,7 +81,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".form-control-profile").on('input', function(){
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        if ($('.pfname').val() == "" || $('.psname').val() == "" || $('.password').val() == "" || !$('.pemail').val().match(re) || $('.pphone').val().length == 0 || $('.pphone').val().length >= 15 ){
+        if ($('.pfname').val() == "" || $('.psname').val() == "" || $('.password').val() == "" || $('.password').val().length < 4 || !$('.pemail').val().match(re) || $('.pphone').val().length == 0 || $('.pphone').val().length >= 15 ){
             $('.pcontinue').addClass('cursor-auto') 
             $('.pcontinue').attr('disabled',true)  
         }
@@ -116,6 +116,19 @@ $(document).ready(function() {
       $(this).toggleClass("fa-eye fa-eye-slash");
 
       var input = $(this).parent().find('.password');
+      if (input.attr("type") === "password") {
+        input.attr("type", "text");
+      } else {
+        input.attr("type", "password");
+      }
+    });
+});
+
+$(document).ready(function() {
+    $("body").on('click', '.toggle-pwd', function() {
+      $(this).toggleClass("fa-eye fa-eye-slash");
+
+      var input = $(this).parent().find('.pwd');
       if (input.attr("type") === "password") {
         input.attr("type", "text");
       } else {
